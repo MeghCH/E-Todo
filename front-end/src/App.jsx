6b30aee7c6933989c9af7860bf1e01b720653a26
Meghan <mcrescihumann@gmail.com>
2025-11-04 17:33:06 +0100

    component parents connexion

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="bg-gray-200">ok</div>;
+  return (
+    <div className="min-h-screen bg-linear-to-bl from-purple-500 to-indigo-600 flex items-center justify-center">
+      <div className="w-104 h-104 bg-gray-200 rounded-xl shadow-lg flex items-center justify-center">
+        <div className="mt-4 text-grey-600 text-x">Connexion</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
