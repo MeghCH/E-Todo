1c9efec98f404815274ca9891b7243e547d40b81
seyf <seyf.sfaxi@epitech.eu>
2025-11-17 12:42:32 +0100

    fonctionne mieux

--- e-todo.sql
+++ e-todo.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS todo (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     due_time DATETIME,
     status VARCHAR(50) DEFAULT 'not started',
     user_id INT NOT NULL,
