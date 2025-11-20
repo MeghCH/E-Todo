69c6a8c8fd45cc021dd0278b2b5419320a85e84c
Meghan <mcrescihumann@gmail.com>
2025-11-20 10:19:53 +0100

    daltonien

---
+++ front-end/src/components/ButtonDaltonien.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { RiEyeFill, RiEyeOffFill } from "@remixicon/react";
+
+export default function ButtonDaltonien() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleChange = (newValue) => {
+    document.body.classList.toggle("daltonien", newValue);
+    localStorage.setItem("daltonienMode", newValue ? "true" : "false");
+    setIsActive(newValue);
+  };
+
+  useEffect(() => {
+    const saved = localStorage.getItem("daltonienMode");
+    if (saved) {
+      const savedIsActive = saved === "true";
+      handleChange(savedIsActive);
+    }
+  }, []);
+
+  return (
+    <button
+      onClick={() => handleChange(!isActive)}
+      className="size-12 hover:opacity-80 flex justify-center items-center rounded-lg bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-white cursor-pointer"
+      title="Mode daltonien"
+      aria-pressed={isActive}
+      aria-label={
+        isActive ? "Désactiver le mode daltonien" : "Activer le mode daltonien"
+      }
+    >
+      {isActive ? <RiEyeOffFill size={22} /> : <RiEyeFill size={22} />}
+    </button>
+  );
+}
